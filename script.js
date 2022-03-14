d588a7dbc5dac5b62fa2ba7a0a70a119e476203a
Matteo Gioele Cocomazzi <TheZefyro@gmail.com>
2022-03-14 16:08:46 +0100

    push con bonus

--- script.js
+++ script.js
@@ -3,17 +3,37 @@ const root = new Vue(
         el: '#root',
         data: {
             albums: [],
+            listaGeneri: [],
+            selectedGenre: '',
+        },
+
+        computed:{
+            filteredAlbum(){
+                if(this.selectedGenre == ''){
+                    return this.albums;
+                }else{
+                    return this.albums.filter(element =>{
+                    return element.genre == this.selectedGenre;
+                    }); 
+                } 
+            }
         },
 
         methods:{
             getAlbum(){
                 // Make a request for a user with a given ID
-                axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+                axios.get('http://localhost/Ercizi%20Boolean%20PHP/php-ajax-dischi/server.php')
                 .then((response) => {
                     // handle success
-                    this.albums = response.data.response;
+                    this.albums = response.data.albums;
                     console.log(this.albums);
                     
+                    for (let i = 0; i < this.albums.length; i++) {
+                        if(!this.listaGeneri.includes(response.data.albums[i].genre)){
+                            this.listaGeneri.push(response.data.albums[i].genre);    
+                        } 
+                    }
+                    console.log(this.listaGeneri);
                 })
                 .catch(function (error) {
                     // handle error
